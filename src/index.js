d612648537aee5ff62999006fc77bd3d37d30424
mayanksingh-tech <kingrocco67@gmail.com>
2020-09-08 00:05:15 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner'
+
+class App extends React.Component {
+
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+
+    renderConatent(){
+        if (this.state.lat && !this.state.errorMessage) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        if (!this.state.lat && this.state.errorMessage) {
+            return <div>Error:{this.state.errorMessage} </div>
+        }
+
+        return <Spinner message='Please accept the location' />;
+    };
+
+
+    render() {
+        return (
+        <div className='border red'>
+            {this.renderConatent()}
+        </div>);
+
+    }
+}
+
+ReactDOM.render(
+    <App />, document.querySelector('#root')
+);
+
